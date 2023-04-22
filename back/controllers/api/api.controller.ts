183b0842489fd183277af66e7af761d327f12583
GregoireARichard <gregoire.richard@hetic.net>
2023-04-22 20:32:42 +0200

    feat: pagination and style along with API edit

--- back/controllers/api/api.controller.ts
+++ back/controllers/api/api.controller.ts
@@ -5,11 +5,9 @@ import { ApiService } from "../../services/index.js";
 
 export default class ApiController{
     public static async sendDataFromPage(req: express.Request, res: express.Response){
-        const page = typeof (req.query.page) == 'string' ? parseInt(req.query.page) : 0
-        const limit = typeof (req.query.limit) == 'string' ? parseInt(req.query.limit) : 0
         const order = typeof (req.query.order) == 'string' ? req.query.order : ""
         const sortAttr = typeof (req.query.sortAttr) == 'string' ? req.query.sortAttr : ""
-        const result =  await ApiService.sendDataFromPage(page, limit, order, sortAttr)
+        const result =  await ApiService.sendDataFromPage(order, sortAttr)
         res.status(200).json(result).end();
     }
 }
--- front/pagination/src/App.js
+++ front/pagination/src/App.js
@@ -1,57 +1,150 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Pagination } from 'react-bootstrap';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Table, Pagination } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [limit, setLimit] = useState(10);
+  const [order, setOrder] = useState("desc");
+  const [sortAttr, setSortAttr] = useState("title");
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'http://localhost:8000/pagination/?page=2&limit=3&order=desc&sortAttr=title',
+        `http://localhost:8000/pagination/?order=${order}&sortAttr=${sortAttr}`
       );
       setData(result.data);
     };
     fetchData();
-  }, []);
+  }, [currentPage, limit, order, sortAttr]);
+
   const handlePageChange = (page) => setCurrentPage(page);
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+  const handleLimitChange = (e) => {
+    setLimit(parseInt(e.target.value));
+    setCurrentPage(1);
+    setItemsPerPage(parseInt(e.target.value));
+  };
 
+  const handleSortChange = (attr) => {
+    if (sortAttr === attr) {
+      setOrder(order === "desc" ? "asc" : "desc");
+    } else {
+      setSortAttr(attr);
+      setOrder("desc");
+    }
+    setCurrentPage(1);
+  };
+
+  const currentItems = data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const pageButtons = [];
+  for (let i = 1; i <= totalPages; i++) {
+    if (
+      i === 1 ||
+      i === currentPage - 1 ||
+      i === currentPage ||
+      i === currentPage + 1 ||
+      i === totalPages
+    ) {
+      pageButtons.push(
+        <Pagination.Item
+          key={i}
+          active={i === currentPage}
+          onClick={() => handlePageChange(i)}
+          style={{ borderRadius: "0", minWidth: "40px", minHeight: "40px", margin: "5px" }}
+        >
+          {i}
+        </Pagination.Item>
+      );
+    }
+  }
+
+  const previousPageButton = (
+    <Pagination.Prev
+      disabled={currentPage === 1}
+      onClick={() => handlePageChange(currentPage - 1)}
+      style={{ borderRadius: "0", minWidth: "40px", minHeight: "40px", margin: "5px" }}
+    />
+  );
+
+  const nextPageButton = (
+    <Pagination.Next
+      disabled={currentPage === totalPages}
+      onClick={() => handlePageChange(currentPage + 1)}
+      style={{ borderRadius: "0", minWidth: "40px", minHeight: "40px", margin: "5px" }}
+    />
+  );
 
   return (
     <>
+      <div className="limit-input">
+        <label htmlFor="limit-input">Limit per page: </label>
+        <input
+          type="number"
+          min="1"
+          max="100"
+          value={limit}
+          onChange={handleLimitChange}
+          id="limit-input"
+          style={{ marginLeft: "5px" }}
+        />
+      </div>
+      <p>Number of Pages is : {Math.ceil(data.length / limit)}</p>
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>User ID</th>
-            <th>Post ID</th>
-            <th>Title</th>
-            <th>Body</th>
+            <th>Rental rate</th>
+            <th>Category</th>
+            <th onClick={() => handleSortChange("title")}>
+              Title {sortAttr === "title" && (order === "desc" ? "▼" : "▲")}
+            </th>
+
+            <th onClick={() => handleSortChange("rating")}>
+              Rating {sortAttr === "rating" && (order === "desc" ? "▼" : "▲")}
+            </th>
+            <th>Inventory id</th>
+            <th onClick={() => handleSortChange("nbOfRentals")}>
+              Number of rentals{" "}
+              {sortAttr === "nbOfRentals" && (order === "desc" ? "▼" : "▲")}
+            </th>
           </tr>
         </thead>
         <tbody>
           {currentItems.map((item) => (
             <tr key={item.id}>
-              <td>{item.userId}</td>
-              <td>{item.id}</td>
+              <td>{item.rental_rate}</td>
+              <td>{item.name}</td>
               <td>{item.title}</td>
-              <td>{item.body}</td>
+              <td>{item.rating}</td>
+              <td>{item.inventory_id}</td>
+              <td>{item.nbOfRentals}</td>
             </tr>
           ))}
         </tbody>
+        
       </Table>
       <Pagination
         className="justify-content-center"
         itemsPerPage={itemsPerPage}
-        totalItems={data.length}
+        totalPages={totalPages}
         currentPage={currentPage}
         onPageChange={handlePageChange}
-      />
+      >
+        <Pagination.Prev
+          disabled={currentPage === 1}
+          onClick={() => handlePageChange(currentPage - 1)}
+        />
+        {pageButtons}
+        <Pagination.Next
+          disabled={currentPage === totalPages}
+          onClick={() => handlePageChange(currentPage + 1)}
+        />
+      </Pagination>
     </>
   );
 }
